29a14333372fd076db02bcc75e0ae0d36a025c52
Tyler Huffman <ty.huff24@gmail.com>
2020-08-11 21:04:07 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,23 @@
 class Owner
   # code goes here
+  attr_accessor  :pets
+  attr_reader :name, :species
+
+  
+  def initialize(name)
+    self.name = name
+    self.species = human
+end
+
+def say_species
+  puts "I am a #{self.species}"
+end
+
+def buy_cat(name)
+  cat = Cat.new(name)
+  cat.owner = self
+end
+
+def buy_dog(name)
+  dog = dog.new(name)
 end

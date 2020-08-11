2cf11fe37dc75d012804d24d8831fd478752cd9d
Tyler Huffman <ty.huff24@gmail.com>
2020-08-11 21:10:05 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,11 @@
 class Cat
   # code goes here
+  attr_accessor :owner, :mood
+attr_reader :name
+
+  @@all = []
+  def initialize(name, owner)
+    self.name = name
+    self.owner = owner
+    @@all << self
 end
